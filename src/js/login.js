25413d88c10bed07e618a345250351ca0af97203
SoyNar <narciris.tech@gmail.com>
2025-03-22 12:45:24 -0500

    feat: add funtionalitie

---
+++ src/js/login.js
@@ -0,0 +1,16 @@
+
+//capturar el formulario
+const form = document.getElementById("login-form")
+
+//procesar los datos
+
+form.addEventListener('submit', function (event))
+{
+    event.preventDefault();
+    
+
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+
+
+}
