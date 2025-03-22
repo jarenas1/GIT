13ade91bb208165e38abd15636dbbf6d724c1711
Nar Mena <narciris.tech@gmail.com>
2025-03-22 13:52:02 -0500

    feat: finally login funtionality

--- src/js/login.js
+++ src/js/login.js
@@ -1,16 +1,39 @@
+// Capturar el formulario
+const form = document.getElementById("login-form");
 
-//capturar el formulario
-const form = document.getElementById("login-form")
-
-//procesar los datos
-
-form.addEventListener('submit', function (event))
-{
+// Procesar los datos
+form.addEventListener('submit', function (event) {
     event.preventDefault();
     
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-    const username = document.getElementById('username');
-    const password = document.getElementById('password');
+    console.log("Usuario:", username);
+    console.log("Contraseña:", password);
 
+    fetch("../js/database.json")
+        .then(response => {
+            if (!response.ok) {  
+                throw new Error("No se puede cargar el archivo JSON");
+            }
+            return response.json();  
+        })
+        .then(usuarios => {
+            let usuarioValido = usuarios.find(user =>
+                 user.username === username && user.password === password);
 
-}
+            if (usuarioValido) {
+                document.getElementById("message").textContent = "Inicio de sesión exitoso.";
+                localStorage.setItem("usuario", username);
+                setTimeout(() => {
+                    window.location.href = "../pages/pages.html"; 
+                }, 1000);
+            } else {
+                document.getElementById("message").textContent = "Usuario o contraseña incorrectos.";
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            document.getElementById("message").textContent = "Error al cargar los datos.";
+        });
+});
