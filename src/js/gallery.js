6c29ead805a3dd391bb54d01b4771859219cb9b7
Pablo Ruiz <pablo_ruiza@hotmail.com>
2025-03-22 12:35:26 -0500

    Databade is created

---
+++ src/js/gallery.js
@@ -0,0 +1,18 @@
+const data = [
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Juanjo", age: 3 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Sofía", age: 5 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Carlos", age: 7 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Lucía", age: 6 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Miguel", age: 4 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Valentina", age: 8 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Andrés", age: 9 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Camila", age: 5 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Diego", age: 6 },
+    { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2hlmMJGK0QxhFLCPChakDzerC-FMgi6Z2tA&s", name: "Elena", age: 7 }
+];
+
+
+
+const section = document.getElementsByTagName('section');
+
+section.ap
